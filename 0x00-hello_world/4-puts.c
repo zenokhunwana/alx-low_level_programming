4093fe46e537e24d369a39849eb9770fcf58515c
zenokhunwana <zenokhunwana@gmail.com>
2023-04-03 04:06:39 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out "Programming is like building a multilingual puzzle
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
