ed541ab9b1c025123472b4339c5df1e3e4e58718
zenokhunwana <zenokhunwana@gmail.com>
2023-06-15 06:06:11 -0700

    c-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+* Return: 1
+*/
+int main(void)
+{
+        write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n",59);
+        return (1);
+}
