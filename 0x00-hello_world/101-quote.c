859fd0ef5d4cbbd1a7de3010296f0e190eaa2d71
zenokhunwana <zenokhunwana@gmail.com>
2023-06-19 04:25:37 -0700

    final script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
-* main - print the string in the put function
 *
 * Description: using the main function
-* this program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+* this program prints and that piece of art is useful - Dora Korpar, 2015-10-19
 * Return: 1
 */
 int main(void)
 {
-        write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
         return (1);
 }
