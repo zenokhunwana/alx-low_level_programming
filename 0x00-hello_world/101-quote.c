1977a850bf0aaf862933c85221c71a4666fd83c0
zenokhunwana <zenokhunwana@gmail.com>
2023-06-19 17:39:20 -0700

    final edits

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 /**
-*
-* Description: using the main function
-* this program prints and that piece of art is useful - Dora Korpar, 2015-10-19
-* Return: 1
-*/
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
