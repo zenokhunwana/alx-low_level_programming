42c194dfd6b422f61c82873ec8490aa6b31157a1
zenokhunwana <zenokhunwana@gmail.com>
2023-06-25 17:06:28 -0700

    2-aplhabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - print alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+char j = 'M';
+char out = tolower(j);
+putchar(out);
+putchar('\n');
+
+return (0);
+}
