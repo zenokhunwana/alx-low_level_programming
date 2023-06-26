9a4df30332119018d92a07c2396db1e2468f0d36
zenokhunwana <zenokhunwana@gmail.com>
2023-06-25 17:30:38 -0700

    corrections 2-alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,12 @@
  */
 int main(void)
 {
-char j = 'M';
-char out = tolower(j);
+char ch;
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+char out = tolower(ch);
 putchar(out);
 putchar('\n');
-
+}
 return (0);
 }
