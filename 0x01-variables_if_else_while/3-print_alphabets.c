98b9c2d984b9f7bb3aaaba72a413234710817923
zenokhunwana <zenokhunwana@gmail.com>
2023-06-25 18:21:12 -0700

    correct

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,11 +14,11 @@ for (ch = 'A'; ch <= 'Z'; ch++)
 char out = tolower(ch);
 putchar(out);
 }
-putchar('\n');
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 up = toupper(ch);
 putchar(up);
 }
+putchar('\n');
 return (0);
 }
