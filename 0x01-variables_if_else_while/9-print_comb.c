798188ceeae7e3dcc8929f83f56e8e2603d9aefc
zenokhunwana <zenokhunwana@gmail.com>
2023-06-28 06:46:22 -0700

    jffb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,11 @@ int main(void)
 int num;
 for (num = 0; num <= 9; num++)
 {
+putchar(num + '0');	
 if (num <= 8)
 {
-putchar(num + '0');
 putchar(',');
 putchar(' ');
-}
-else
-{
-putchar(num + '0');
 }
 }
 putchar('\n');
