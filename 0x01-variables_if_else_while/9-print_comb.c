243094e6bdf6e3cf05d877281db5544d40744b0b
zenokhunwana <zenokhunwana@gmail.com>
2023-06-28 05:59:16 -0700

    correct

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,16 @@ int main(void)
 int num;
 for (num = 0; num <= 9; num++)
 {
+if (num <= 8)
+{
 putchar(num + '0');
 putchar(',');
 putchar(' ');
+}
+else
+{
+putchar(num + '0');
+}
 }
 return (0);
 }
