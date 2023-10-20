f19f252d20019b8bce7fd65e7d7387419a55e5c1
zenokhunwana <zenokhunwana@gmail.com>
2023-10-20 03:26:49 -0700

    100-corrections

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,9 @@ void printMessage(void) __attribute__ ((constructor));
  *
  */
 
-void printMessage(void) {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+void printMessage(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
 
